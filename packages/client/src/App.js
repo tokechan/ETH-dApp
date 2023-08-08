7cb8c5d9228e1fba04d0a0de48d115bb3bd40b9e
tokec <fleatoke@gmail.com>
2023-08-08 19:03:51 +0900

    upload to github

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -8,23 +8,26 @@ const App = () => {
   const [messageValue, setMessageValue] = useState("");
   const [allWaves, setAllWaves] = useState([]);
   console.log("currentAccount: ", currentAccount);
-  const contractAddress = "0x1F028b6fda403fd4965a8Bb83B4B70AD9C4d1057";
+
+  const contractAddress = "0x7D62871e827312395F015a904B512105f38bB571";
+
   const contractABI = abi.abi;
 
   const getAllWaves = async () => {
     const { ethereum } = window;
 
-    try {
+    try{
       if (ethereum) {
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
         const wavePortalContract = new ethers.Contract(
           contractAddress,
           contractABI,
-          signer
+          signer,
         );
-
+        
         const waves = await wavePortalContract.getAllWaves();
+
         const wavesCleaned = waves.map((wave) => {
           return {
             address: wave.waver,
@@ -61,34 +64,35 @@ const App = () => {
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const signer = provider.getSigner();
 
-      const wavePortalContract = new ethers.Contract(
-        contractAddress,
-        contractABI,
-        signer
-      );
-      wavePortalContract.on("NewWave", onNewWave);
-    }
+      wavePortalContract = new ethers.Contract(
+      contractAddress,
+      contractABI,
+      signer
+    );
+    wavePortalContract.on("NewWave", onNewWave);
+  }
 
-    return () => {
-      if (wavePortalContract) {
-        wavePortalContract.off("NewWave", onNewWave);
-      }
-    };
+  return () => {
+    if (wavePortalContract) {
+      wavePortalContract.off("NewWave", onNewWave);
+    }
+  };
+
   }, []);
 
   const checkIfWalletIsConnected = async () => {
     try {
       const { ethereum } = window;
-      if (!ethereum) {
+      if(!ethereum) {
         console.log("Make sure you have MetaMask!");
         return;
       } else {
-        console.log("We have the ethereum object", ethereum);
+        console.log("We have the ethereum object", ethereum); 
       }
       const accounts = await ethereum.request({ method: "eth_accounts" });
       if (accounts.length !== 0) {
         const account = accounts[0];
-        console.log("Found an authorized account: ", account);
+        console.log("Found an authorized account:", account);
         setCurrentAccount(account);
       } else {
         console.log("No authorized account found");
@@ -105,9 +109,7 @@ const App = () => {
         alert("Get MetaMask!");
         return;
       }
-      const accounts = await ethereum.request({
-        method: "eth_requestAccounts",
-      });
+      const accounts = await ethereum.request({ method: "eth_requestAccounts",});
       console.log("Connected: ", accounts[0]);
       setCurrentAccount(accounts[0]);
     } catch (error) {
@@ -132,107 +134,96 @@ const App = () => {
         let contractBalance = await provider.getBalance(wavePortalContract.address);
         console.log("Contract balance:", ethers.utils.formatEther(contractBalance));
 
-        const waveTxn = await wavePortalContract.wave(messageValue, {
-          gasLimit: 300000,
-        });
+        const waveTxn = await wavePortalContract.wave(messageValue, {gasLimit: 300000});
         console.log("Mining...", waveTxn.hash);
         await waveTxn.wait();
-        console.log("Mined -- ", waveTxn.hash);
+        console.log("Mined -- ",waveTxn.hash);
         count = await wavePortalContract.getTotalWaves();
         console.log("Retrieved total wave count...", count.toNumber());
 
-        let contractBalance_post = await provider.getBalance(
-          wavePortalContract.address
-        );
-
-        if (contractBalance_post.lt(contractBalance)) {
-          console.log("User won ETH!");
+        // let contractBalance = await provider.getBalance(wavePortalContract.address);
+        let contractBalance_post = await provider.getBalance(wavePortalContract.address);
+        console.log("Contract balance:", ethers.utils.formatEther(contractBalance));
+        if(contractBalance_post.lt(contractBalance)) {
         } else {
-          console.log("User didn't win ETH.");
+          console.log("User won ETH!");
         }
-        console.log(
-          "Contract balance after wave:",
-          ethers.utils.formatEther(contractBalance_post)
-        );
+        console.log("Contract balance after wave:", ethers.utils.formatEther(contractBalance_post));
       } else {
         console.log("Ethereum object doesn't exist!");
       }
-    } catch (error) {
-      console.log(error)
-    }
-  };
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+
 
   useEffect(() => {
     checkIfWalletIsConnected();
   }, []);
-
   return (
     <div className="mainContainer">
       <div className="dataContainer">
         <div className="header">
-          <span role="img" aria-label="hand-wave">
-            👋
+          <span role="img" aria-label="handpress">
+            🙏🏻
           </span>{" "}
           WELCOME!
         </div>
         <div className="bio">
-          イーサリアムウォレットを接続して、メッセージを作成したら、
-          <span role="img" aria-label="hand-wave">
-            👋
+          イーサリウムウォレットを接続して、「
+          <span role="img" aria-label="handpress">
+            🙏🏻
           </span>
-          を送ってください
-          <span role="img" aria-label="shine">
-            ✨
+          (wave)」を送ってくれい
+          <span role="img" aria-label="tidalwave">
+            🌊
           </span>
         </div>
         <br />
-        { }
         {!currentAccount && (
           <button className="waveButton" onClick={connectWallet}>
-            Connect Wallet
+          Connect Wallet
           </button>
         )}
         {currentAccount && (
-          <button className="waveButton" onClick={connectWallet}>
-            Wallet Connected
-          </button>
+          <button className="waveButton">Wallet Connected</button>
         )}
-        { }
         {currentAccount && (
-          <button className="waveButton" onClick={wave}>
-            Wave at Me
-          </button>
+        <button className="waveButton" onClick={wave}>
+          Wave at Me
+        </button>
         )}
-        { }
         {currentAccount && (
           <textarea
             name="messageArea"
-            placeholder="メッセージはこちら"
+            placeholder="Message is here or where?"
             type="text"
             id="message"
             value={messageValue}
             onChange={(e) => setMessageValue(e.target.value)}
-          />
+            />
         )}
-        {currentAccount &&
-          allWaves
-            .slice(0)
-            .reverse()
-            .map((wave, index) => {
-              return (
-                <div
+            {currentAccount &&
+              allWaves
+              .slice(0)
+              .reverse()
+              .map((wave, index) => {
+                return (
+                  <div
                   key={index}
                   style={{
-                    backgroundColor: "#F8F8FF",
+                    backgroundColor: "#F898FF",
                     marginTop: "16px",
                     padding: "8px",
                   }}
-                >
-                  <div>Address: {wave.address}</div>
-                  <div>Time: {wave.timestamp.toString()}</div>
-                  <div>Message: {wave.message}</div>
-                </div>
-              );
+                  >
+                    <div>Address: {wave.address}</div>
+                    <div>Time: {wave.timestamp.toString()}</div>
+                    <div>Message: {wave.message}</div>
+                  </div>
+                );
             })}
       </div>
     </div>
